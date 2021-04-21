81e59bde3e2a8796db7dd638c024fdbcdc99e028
cagatay.cali <cagatay.cali@trendyol.com>
2021-04-21 22:57:34 +0300

    🎉  Init

---
+++ index.js
@@ -0,0 +1,6 @@
+// Kata: https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/javascript
+const incrementString = string => {
+
+}
+
+module.exports = incrementString
---
+++ index.test.js
@@ -0,0 +1,7 @@
+const incrementString = require('./index')
+
+describe('incrementString', () => {
+  it('2 to be 2', () => {
+    expect(2).toBe(2)
+  })
+})
